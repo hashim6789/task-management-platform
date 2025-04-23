e91f407e1a5bce3ca9310b81e04ce2007c2a01ba
hashim6789 <muhammedhashim6789@gmail.com>
2025-04-23 14:08:51 +0530

    testing

--- backend/src/utils/refresh-cookie.util.ts
+++ backend/src/utils/refresh-cookie.util.ts
@@ -4,16 +4,20 @@ import { Response } from "express";
 export function setCookie(res: Response, refreshToken: string, entity: string) {
   res.cookie(entity, refreshToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    sameSite: env.NODE_ENV === "production" ? "none" : "strict",
+    secure: false,
+    sameSite: "none",
+    // secure: process.env.NODE_ENV === "production",
+    // sameSite: env.NODE_ENV === "production" ? "none" : "strict",
   });
 }
 
 export function deleteCookie(res: Response, entity: string) {
   res.clearCookie(entity, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: env.NODE_ENV === "production" ? "none" : "strict",
+    secure: false,
+    sameSite: "none",
+    // secure: process.env.NODE_ENV === "production",
+    // sameSite: env.NODE_ENV === "production" ? "none" : "strict",
   });
 }
