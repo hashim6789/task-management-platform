65a2f286ba3e96639891e5069cab660492ee7a39
hashim6789 <muhammedhashim6789@gmail.com>
2025-04-24 12:21:52 +0530

    fix (backend) : The auth router issue is resolved

---
+++ backend/src/routers/auth.route.ts
@@ -0,0 +1,42 @@
+import { AuthController } from "@/controllers/implementation";
+import { IAuthController } from "@/controllers/interface";
+import {
+  validate,
+  validateBlockedOrNot,
+  verifyTokenMiddleware,
+} from "@/middlewares";
+import UserModel from "@/models/user.model";
+import { UserRepository } from "@/repositories/implementation";
+import { IUserRepository } from "@/repositories/interface";
+import { loginSchema } from "@/schema";
+import { AuthService } from "@/services/implementation";
+import { IAuthService } from "@/services/interface";
+import { Router } from "express";
+
+const authRouter = Router();
+
+const userRepository: IUserRepository = new UserRepository(UserModel);
+const authService: IAuthService = new AuthService(userRepository);
+const authController: IAuthController = new AuthController(authService);
+
+authRouter.post(
+  "/refresh",
+  authController.refreshAccessToken.bind(authController)
+);
+
+authRouter.get(
+  "/me",
+  validateBlockedOrNot(),
+  verifyTokenMiddleware(["admin", "user"]),
+  authController.me.bind(authController)
+);
+
+authRouter.post(
+  "/login",
+  validateBlockedOrNot(),
+  validate(loginSchema),
+  authController.login.bind(authController)
+);
+authRouter.post("/logout", authController.logout.bind(authController));
+
+export { authRouter };
