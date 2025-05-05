290c0c48e873a5d656ae8528539f746d1c93b981
hashim6789 <muhammedhashim6789@gmail.com>
2025-05-05 12:23:58 +0530

    refactor (fornt end) : The env config file is changed

--- frontend/src/configs/env.config.ts
+++ frontend/src/configs/env.config.ts
@@ -1,8 +1,8 @@
 export const env = {
   get SERVER_ORIGIN() {
     // return import.meta.env.VITE_SERVER_ORIGIN;
-    return "http://localhost";
-    // return "http://3.110.92.5";
+    // return "http://localhost";
+    return "http://3.111.41.75";
   },
 };
 
